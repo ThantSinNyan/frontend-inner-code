848cfe73a67cab936f68244e861e2d4ac7ae13d2
Min Pyae Hein <minpyaehein536h@prestigein.com>
2025-08-24 16:36:16 +0700

    fix buid fail

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia'
 import piniaPersistedstate from 'pinia-plugin-persistedstate'
-import router from './router'
+import router from './router';
 import i18n from './i18n'
 
 const app = createApp(App)

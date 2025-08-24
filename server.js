bc01f2946e82af92a7ebb86bcbcdb8e0fd15af28
Min Pyae Hein <minpyaehein536h@prestigein.com>
2025-08-24 20:50:19 +0700

    add config for heroku

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from dist
+app.use(express.static(path.join(__dirname, "dist")));
+
+// Handle SPA (Single Page App)
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
